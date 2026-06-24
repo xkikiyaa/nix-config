a5c28960e762c2e07da394d27e8a5996504cd8e1
Lesley Litjens <omgkawaiiangel077@gmail.com>
2026-06-24 22:16:55 +0200

    update

--- modules/etc.nix
+++ modules/etc.nix
@@ -5,7 +5,8 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   networking.hostName = "nixos";
-
+  
+  programs.nix-ld.enable = true;
 
   fileSystems."/mnt/1TB" = {
   device = "UUID=109cddd7-cfdc-46c4-beff-698b7a1078c6";
