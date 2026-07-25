94939eccb2923fe8b93f153f274bb5ab6430819c
Lesley Litjens <omgkawaiiangel077@gmail.com>
2026-07-26 01:43:55 +0200

    update

---
+++ modules/.sddm-kath.nix
@@ -0,0 +1,62 @@
+{ pkgs, ... }:
+
+#let
+#  sddm-astronaut = pkgs.sddm-astronaut.override {
+#    embeddedTheme = "hyprland_kath";
+#
+#    themeConfig = {
+#      HeaderTextColor = "#d5c4a1";
+#    };
+#  };
+#
+#  sddm-xsetup = pkgs.writeShellScript "sddm-xsetup" ''
+#    ${pkgs.xrandr}/bin/xrandr --output DP-3 --off || true
+#  '';
+#in
+{
+  services.xserver.enable = true;
+
+  services.displayManager.sddm = {
+    enable = true;
+    theme = "sddm-astronaut-theme";
+    autoNumlock = true;
+    settings = {
+      General = {
+        DisplayServer = "x11";
+      };
+
+      X11 = {
+        DisplayCommand = "${sddm-xsetup}";
+      };
+    };
+
+    extraPackages = with pkgs; [
+      sddm-astronaut
+      kdePackages.qtmultimedia
+      xrandr
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    sddm-astronaut
+    kdePackages.qtmultimedia
+    xrandr
+  ];
+
+  services.desktopManager.plasma6.enable = true;
+
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    plasma-browser-integration
+    konsole
+    elisa
+    discover
+    okular
+    qrca
+    kate
+  ];
+
+  environment.etc."xdg/kcminputrc".text = ''
+    [Keyboard]
+    NumLock=0
+  '';
+}
--- modules/plasma-ly.nix
+++ modules/plasma-ly.nix
@@ -1,49 +1,21 @@
 { pkgs, ... }:
 
-let
-  sddm-astronaut = pkgs.sddm-astronaut.override {
-    embeddedTheme = "hyprland_kath";
-
-    themeConfig = {
-      HeaderTextColor = "#d5c4a1";
-    };
-  };
-
-  sddm-xsetup = pkgs.writeShellScript "sddm-xsetup" ''
-    ${pkgs.xrandr}/bin/xrandr --output DP-3 --off || true
-  '';
-in
 {
   services.xserver.enable = true;
 
-  services.displayManager.sddm = {
+  services.displayManager.ly = {
     enable = true;
-    theme = "sddm-astronaut-theme";
-    autoNumlock = true;
     settings = {
-      General = {
-        DisplayServer = "x11";
-      };
-
-      X11 = {
-        DisplayCommand = "${sddm-xsetup}";
-      };
+      numlock = true;
+      lang = "nl";
     };
-
-    extraPackages = with pkgs; [
-      sddm-astronaut
-      kdePackages.qtmultimedia
-      xrandr
-    ];
   };
 
-  environment.systemPackages = with pkgs; [
-    sddm-astronaut
-    kdePackages.qtmultimedia
-    xrandr
-  ];
-
   services.desktopManager.plasma6.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    ly
+  ];
 
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
     plasma-browser-integration
@@ -54,9 +26,4 @@ in
     qrca
     kate
   ];
-
-  environment.etc."xdg/kcminputrc".text = ''
-    [Keyboard]
-    NumLock=0
-  '';
 }
